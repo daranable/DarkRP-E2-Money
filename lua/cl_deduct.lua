50c1f10681ac8d87d54d64e9f5ed819481c81a4b
Robert Avery <daranable@gmail.com>
2012-08-25 23:36:47 -0500

    Added some client side lua for askForMoney.

---
+++ lua/cl_deduct.lua
@@ -0,0 +1,42 @@
+function openRequestMenu( data )
+	local asker = data:ReadEntity()
+	local requestnum = data:ReadLong()
+	local amount = data:ReadLong()
+	local message = data:ReadString()
+	
+	local DeductFrame = vgui.Create( "DFrame" )
+	DeductFrame:SetSize( 250,  200 )
+	DeductFrame:SetPos( ( ScrW() / 2 ) - 125, ( ScrH() / 2 ) - 75 )
+	DeductFrame:SetTitle( asker:Nick() .. " is asking you for money" )
+	DeductFrame:SetVisible( true )
+	DeductFrame:ShowCloseButton( true )
+	DeductFrame:MakePopup()
+	
+	local DeductLabel = Label(
+		"The Precessor of " .. asker:Nick() .. " is asking \nyou for $" .. 
+		tostring(amount) .. 
+		"\nWould you like to accept?",
+		DeductFrame
+	)
+	DeductLabel:SizeToContents()
+	DeductLabel:SetPos( 10, 30 )
+	
+	local DeductYes = vgui.Create( "DButton", DeductFrame )
+	DeductYes:SetPos( 10, 100 )
+	DeductYes:SetSize( 100, 40 )
+	DeductYes:SetText( "Yes" )
+	DeductYes.DoClick = function()
+		RunConsoleCommand( "drp_money_request", "accept", requestnum )
+		DeductFrame:Close()
+	end
+	
+	local DeductNo = vgui.Create( "DButton", DeductFrame )
+	DeductNo:SetPos( 135, 100 )
+	DeductNo:SetSize( 100, 40 )
+	DeductNo:SetText( "No" )
+	DeductNo.DoClick = function()
+		RunConsoleCommand( "drp_money_request", "decline", requestnum )
+		DeductFrame:Close()
+	end
+end
+usermessage.Hook( "e2deduct", openRequestMenu )
